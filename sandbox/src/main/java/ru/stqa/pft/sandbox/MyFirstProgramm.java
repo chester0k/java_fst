aff40d00a7df41bb1ca741246ede76cff24f30e7
Evgeny <j7.odessa@gmail.com>
2018-08-12 12:18:56 +0300

    plussminus

---
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgramm.java
@@ -0,0 +1,21 @@
+package ru.stqa.pft.sandbox;
+
+public class MyFirstProgramm {
+
+    public static void main(String[] args) {
+        System.out.println("Hello, world!");
+        System.out.println(2 + 2);
+        System.out.println(2 * 2);
+        System.out.println(2 / 2);
+        System.out.println(2 - 2);
+
+        System.out.println(1 / 2);
+        System.out.println(1.0 / 2);
+        System.out.println(1 / 2.0);
+        System.out.println("2" + "2");
+        System.out.println("2" + 2);
+        System.out.println((2 + 2) * 2);
+        System.out.println("2 + 2 = " + (2 + 2));
+    }
+
+}
--- sandbox/src/main/java/ru/stqa/pft/sandbox/myFirstProgramm.java
+++
@@ -1,8 +0,0 @@
-package ru.stqa.pft.sandbox;
-
-public class myFirstProgramm
- {
- public static void main (String[] args) {
- System.out.println("Hello, world!");
- }
- }
