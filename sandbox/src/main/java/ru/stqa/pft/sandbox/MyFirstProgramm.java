04653a260db5baab9582b4853022f901ef8b220a
Evgeny <j7.odessa@gmail.com>
2018-08-13 12:54:44 +0300

    Добавлено несколько функций

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgramm.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgramm.java
@@ -3,19 +3,34 @@ package ru.stqa.pft.sandbox;
 public class MyFirstProgramm {
 
     public static void main(String[] args) {
-        System.out.println("Hello, world!");
-        System.out.println(2 + 2);
-        System.out.println(2 * 2);
-        System.out.println(2 / 2);
-        System.out.println(2 - 2);
+        hello("Pidor");
+        hello("Pidor");
+        hello("Pidor");
 
-        System.out.println(1 / 2);
-        System.out.println(1.0 / 2);
-        System.out.println(1 / 2.0);
-        System.out.println("2" + "2");
-        System.out.println("2" + 2);
-        System.out.println((2 + 2) * 2);
-        System.out.println("2 + 2 = " + (2 + 2));
+        double len = 5;
+        System.out.println("Площадь квадрата со сторой " + len + " = " + area(len) );
+
+        double a = 4;
+        double b = 6;
+        System.out.println("Площадь прямоугольника со сторонами " + a + " и " + b + " = " + area(a, b));
+
+    }
+    public static void hello(String somebody) {
+
+        System.out.println("Hello, " + somebody + '!');
+
+
+        double l = 8;
+        double s = l * l;
+        System.out.println("Площадь квадрата со стороной " + l + " = " + (s * s));
+
+    }
+    public static double area(double l) {
+        return l * l;
+
     }
 
+    public static double area(double a, double b) {
+        return a * b;
+    }
 }
